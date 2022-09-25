3bbf4280744602aee07a0f718eca245accb2a7d1
Paul Chibueze <paulpadro03@gmail.com>
2022-09-25 09:23:36 +0100

    edited 500 error page middleware

--- app.js
+++ app.js
@@ -119,6 +119,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500)
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
